157ec52b622e0867ce94e6380640f349c4fde70d
Eko <ekoremdev@gmail.com>
2020-03-17 11:35:16 +0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'dart:math';
+
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Map<String,bool> score = {};
+
+  final Map choices = {
+    '🍏' : Colors.green,
+    '💛' : Colors.yellow,
+    '🍎' : Colors.red,
+    '🍆' : Colors.purple,
+    '🥔' : Colors.brown,
+    '🥔' : Colors.orange,
+  };
+
+  // random seed to shuffle order of items
+  int seed = 0;
+
+  AudioCache _audioController = AudioCache();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Score ${score.length} / 6'),
+          backgroundColor: Colors.pink),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () {
+          setState(() {
+            score.clear();
+            seed++;
+          });
+        },
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: choices.keys.map((emoji) {
+                return Draggable<String>(
+                  data: emoji,
+                  child: Emoji(emoji: score[emoji] == true ? '✅' : emoji),
+                  feedback: Emoji(emoji: emoji),
+                  childWhenDragging: Emoji(emoji: '🌱'),
+                );
+              }).toList()),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:
+            choices.keys.map((emoji) => _buildDragTarget(emoji)).toList()
+              ..shuffle(Random(seed)),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDragTarget(emoji) {
+    return DragTarget<String>(
+      builder: (BuildContext context, List<String> incoming, List rejected) {
+        if (score[emoji] == true) {
+          return Container(
+            color: Colors.white,
+            child: Text('Correct!'),
+            alignment: Alignment.center,
+            height: 80,
+            width: 200,
+          );
+        } else {
+          return Container(color: choices[emoji], height: 80, width: 200);
+        }
+      },
+      onWillAccept: (data) => data == emoji,
+      onAccept: (data) {
+        setState(() {
+          score[emoji] = true;
+          plyr.play('success.mp3');
+        });
+      },
+      onLeave: (data) {},
+    );
+  }
+}
+
+class Emoji extends StatelessWidget {
+  Emoji({Key key, this.emoji}) : super(key: key);
+
+  final String emoji;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        alignment: Alignment.center,
+        height: 50,
+        padding: EdgeInsets.all(2),
+        child: Text(
+          emoji,
+          style: TextStyle(color: Colors.black, fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
+
+AudioCache plyr = AudioCache();
